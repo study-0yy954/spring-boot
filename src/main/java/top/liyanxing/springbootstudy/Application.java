2b2ebf29f3c24a0564b47e7a8fe0c27015bb97cc
李艳兴 <liyanxingelon@gmail.com>
2023-07-28 15:21:54 +0800

    feat:修改包名

--- src/main/java/top/liyanxing/springbootstudy/Application.java
+++ src/main/java/top/liyanxing/springbootstudy/Application.java
@@ -1,4 +1,4 @@
-package com.lyx;
+package top.liyanxing.springbootstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/top/liyanxing/springbootstudy/controller/MyController.java
+++ src/main/java/top/liyanxing/springbootstudy/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.lyx.controller;
+package top.liyanxing.springbootstudy.controller;
 
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
