29f4ba05c40bec13bc10ff23b217d48cc809ccae
李艳兴 <liyanxingelon@gmail.com>
2023-08-24 16:19:21 +0800

    feat:修改包名

---
+++ src/test/java/top/liyanxing/springbotstudy/ApplicationTest.java
@@ -0,0 +1,24 @@
+package top.liyanxing.springbotstudy;
+
+import cn.hutool.core.lang.Console;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import top.liyanxing.springbootstudy.Application;
+import top.liyanxing.springbootstudy.controller.MyController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+class ApplicationTest
+{
+    @Autowired
+    private MyController myController;
+
+    @Test
+    public void test1()
+    {
+        Console.log(myController);
+    }
+}
