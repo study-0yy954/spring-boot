e078a45923edafab4c6f530964bb56c704124849
leeyx <leeyx@gmail.com>
2024-04-12 13:43:28 +0800

    111

--- src/main/java/top/liyanxing/springbootstudy/controller/MyController.java
+++ src/main/java/top/liyanxing/springbootstudy/controller/MyController.java
@@ -1,7 +1,10 @@
 package top.liyanxing.springbootstudy.controller;
 
+import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.liyanxing.common.CommonResult;
@@ -14,4 +17,12 @@ public class MyController
     {
         return CommonResult.successData(StrUtil.format("成功调用了：{}", IdUtil.fastSimpleUUID()));
     }
+
+    @GetMapping("/foo")
+    public ResponseEntity<String> foo()
+    {
+        Console.log("收到请求");
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("出错了");
+    }
+
 }
