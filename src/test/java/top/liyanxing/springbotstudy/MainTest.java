fc37bdce87d937151fc98aef12a716855e49c24b
leeyx <leeyx@gmail.com>
2024-05-30 16:15:01 +0800

    feat: 学习

--- src/test/java/top/liyanxing/springbotstudy/MainTest.java
+++ src/test/java/top/liyanxing/springbotstudy/MainTest.java
@@ -8,11 +8,11 @@ public class MainTest
 {
     public static void main(String[] args)
     {
-        PasswordEncoder encoder = new BCryptPasswordEncoder(4);
-        String resutl = encoder.encode("123");
+        PasswordEncoder encoder1 = new BCryptPasswordEncoder(4);
+        String resutl1 = encoder1.encode("123");
+        Console.log("输出数据【{}】", resutl1);
 
-        Console.log("输出数据【{}】", resutl);
-
-        encoder.matches("123", resutl);
+        PasswordEncoder encoder2 = new BCryptPasswordEncoder(5);
+        Console.log("输出数据【{}】", encoder2.matches("123", resutl1));
     }
 }
