49003d2841cf40b8fd57a74ec843e97527605368
leeyx <leeyx@gmail.com>
2024-05-31 16:33:13 +0800

    feat: 学习

--- src/main/java/top/liyanxing/springbootstudyuse/config/springsecurity/SecurityAuthenticationFailureHandler.java
+++ src/main/java/top/liyanxing/springbootstudyuse/config/springsecurity/SecurityAuthenticationFailureHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import top.liyanxing.common.CommonResult;
 import top.liyanxing.springbootstudyuse.common.baseclass.ObjectMapperOpt;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/top/liyanxing/springbootstudyuse/config/springsecurity/SecurityAuthenticationSuccessHandler.java
+++ src/main/java/top/liyanxing/springbootstudyuse/config/springsecurity/SecurityAuthenticationSuccessHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import top.liyanxing.common.CommonResult;
 import top.liyanxing.springbootstudyuse.common.baseclass.ObjectMapperOpt;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
